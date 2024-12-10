08075f7bb3b1bff83c6e01824f5bd1d866cdbeb7
kyle moir <98747393+03y@users.noreply.github.com>
2024-12-10 08:12:06 +0000

    interested in how good a bruteforce can be

--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -1,8 +1,10 @@
 import re
-import random
+from datetime import datetime
 
 seed = 0x0000
 magic = 0xFFFF
+cache = {'first': 0, 'last':-1}
+begin_exec = datetime.now()
 
 class Block:
     def __init__(self, size, empty):
@@ -18,16 +20,18 @@ def is_sorted(fs):
     return True
 
 def get_last(fs):
-    pos = len(fs)-1
+    pos = cache['last']
     while pos > 0:
         if fs[pos] != chr(magic):
+            cache['last'] = pos
             return pos
         pos -= 1
     return -1
 
 def get_free(fs):
-    for i in range(len(fs)):
+    for i in range(cache['first'], len(fs)):
         if fs[i] == chr(magic):
+            cache['first'] = i
             return i
     return -1
 
@@ -37,8 +41,6 @@ def get_unicode_char():
     n = seed
     seed += 1
     return chr(n)
-
-print('prepare for O(n^2) solution lmao...')
 
 with open('input.txt') as f:
     input = f.readline().strip('\n')
@@ -61,10 +63,7 @@ with open('input.txt') as f:
             fs += str(id) * blocks[i].size
             id = get_unicode_char()
 
-    def print_fs(fs):
-        for i in range(len(fs)):
-            print(fs[i], end='')
-        print()
+    cache['last'] = len(fs)-1
 
     sorted = False
     while not sorted:
@@ -82,3 +81,26 @@ with open('input.txt') as f:
             checksum += i * ord(fs[i])
 
     print('Part 1:', checksum)
+    
+    print('execution time:', datetime.now()-begin_exec)
+
+'''
+how fast can a brutefoce be?
+benchmark: i9-13950HX, 64GB RAM
+
+Part 1: 1928
+execution time: 0:00:00.000445
+
+Part 1: 6299243228569
+execution time: 0:01:39.892172
+
+implemented start cache:
+
+Part 1: 6299243228569
+execution time: 0:01:15.048865
+
+implemented end cache:
+
+Part 1: 6299243228569
+execution time: 0:00:54.461692
+'''
