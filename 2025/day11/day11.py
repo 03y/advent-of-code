57c8f7dc5da3d952faca71f28d628098b20195ef
kyle moir <98747393+03y@users.noreply.github.com>
2025-12-11 12:22:36 +0000

    day 11 part 2

--- 2025/day11/day11.py
+++ 2025/day11/day11.py
@@ -14,38 +14,41 @@ def digraph_to_adjlist(dot):
     return adj
 
 # count paths using memoisation
-def count_paths(graph, start, end, memo=None):
+def count_paths(graph, parent, goal, memo=None):
     if memo is None:
         memo = {}
-    if start == end:
+    if parent == goal:
         return 1
-    if start in memo:
-        return memo[start]
+    if parent in memo:
+        return memo[parent]
 
     total = 0
-    for neighbour in graph[start]:
-        total += count_paths(graph, neighbour, end, memo)
+    for child in graph[parent]:
+        total += count_paths(graph, child, goal, memo)
 
-    memo[start] = total
+    memo[parent] = total
     return total
 
-graph = Digraph(name='aoc')
+graph = Digraph(name='Advent of Code 2025 day 11')
 
 f = open('input.txt')
 for l in f:
     node_name = l[:l.index(':')]
     node_children = l[l.index(':')+1:].split()
 
-    with graph.subgraph() as Devices:
-        Devices.attr(name=node_name)
-        Devices.node(node_name)
+    with graph.subgraph() as Node:
+        Node.attr(name=node_name)
+        Node.node(node_name)
 
         for child in node_children:
-            Devices.node(child)
+            Node.node(child)
             graph.edge(node_name, child)
 
 # uncomment to render graph
-#graph.render('aoc', view=True)
+#graph.render('Advent of Code 2025 day 11', view=True)
 
-adjlist = digraph_to_adjlist(graph) # convert graphviz object to neighbour dict
-print('Part 1:', count_paths(adjlist, 'you', 'out'))
+nodes = digraph_to_adjlist(graph) # convert graphviz object to neighbour dict
+print('Part 1:', count_paths(nodes, 'you', 'out'))
+print('Part 2:', count_paths(nodes, 'svr', 'fft') *
+                 count_paths(nodes, 'fft', 'dac') *
+                 count_paths(nodes, 'dac', 'out'))
